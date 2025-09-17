84678093dc1576fc13aa64b8117098e977125108
Matthew Sharpe <driedsnow157@gmail.com>
2025-09-17 12:12:37 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   description = "My Home Manager configuration";
 
   inputs = {
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     qsp-flake = {
       url = "git+ssh://git@github.com/DriedSnow/qsp-flake.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      # url = "github:nix-community/home-manager/master";
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
--- home.nix
+++ home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, pkgs-unstable, inputs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "deck";
   home.homeDirectory = "/home/deck";
 
@@ -11,18 +9,8 @@
     ./overlay.nix
   ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # DO NOT CHANGE WITHOUT CHECKING RELEASE NOTES
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  # added the builtins.attrValues and inherit as it is better then with and i dont want to type pkgs. all the time
   home.packages = [
    pkgs.qsp-tools
    pkgs.update-sys
@@ -32,7 +20,6 @@
    pkgs.vlc
    pkgs.mpv
    pkgs.yt-dlp
-   pkgs.xarchiver
    pkgs.lrzip
    pkgs.gnutar
    pkgs.github-cli
@@ -64,22 +51,6 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/deck/etc/profile.d/hm-session-vars.sh
-  #
   programs.bash = {
     enable = true;
     enableCompletion = true;
@@ -165,7 +136,6 @@
     };
   };
 
-  # Let Home Manager install and manage itself.
   targets.genericLinux.enable = true;
   programs.home-manager.enable = true;
 }
